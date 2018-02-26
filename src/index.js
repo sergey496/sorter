04d70814d710e6aeb8388ca5e31deb9145aadb21
Sergey Zylevich <sergey496@yandex.ru>
2018-02-26 12:16:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,49 +1,48 @@
-class Sorter {
-  constructor() {
+class Sorter{
+  constructor() 
+  {
     this.arr = [];
-	this.length=0;
+    this.compare = (a,b) => a-b;
   }
 
-  add(element) {
-	this.length++;
-   this.arr.splice(this.arr.length,0,element);
+  add(element) 
+  {
+	this.arr.push(element)
   }
 
-  at(index) {
-    return this.arr[index];
+  at(index) 
+  {
+    return this.arr[index]
   }
 
-  length() {
-    return this.arr.length;
+  get length() 
+  {
+    return this.arr.length
   }
 
-  toArray() {
-    return this.arr;
+  toArray() 
+  {
+    return this.arr.slice()
   }
 
-  sort(indices) {
-	var tmp=[];
-	for (var i=0; i<indices.length;i++)
-		{
-			tmp[i]=this.arr[indices[i]];
-		}
-		function compareNumeric(a, b) 
-			{
-				if (a > b) return 1;
-				if (a < b) return -1;
-			}
-	tmp.sort(compareNumeric);	
-	indices.sort(compareNumeric);
-  	for (var i=0; i<indices.length;i++)
-		{
-			this.arr[indices[i]]=tmp[i];
-		}
-	return this.arr;
+  sort(indices) 
+  {
+      let subArray = indices.sort().map((index) => this.arr[index]);
+      subArray.sort(this.compare);
+      this.arr.reduce((previousValue, currentItem, index, arr) => 
+	  {
+          if(indices.indexOf(index) > -1)
+			  {
+				arr[index] = subArray[indices.indexOf(index)]
+			  }
+          return arr
+      }, null);
+      return this
   }
 
-  setComparator(compareFunction) {
-
+  setComparator(compareFunction) 
+  {
+    this.compare = compareFunction;
   }
 }
-let sorter = new Sorter("");
 module.exports = Sorter;
