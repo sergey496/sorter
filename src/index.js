7c7ece25fe2af79279ac2a56ee814dfe64999419
Sergey Zylevich <sergey496@yandex.ru>
2018-09-17 15:34:26 +0300

    17.09

--- src/index.js
+++ src/index.js
@@ -1,48 +1,37 @@
 class Sorter{
-  constructor() 
-  {
-    this.arr = [];
-    this.compare = (a,b) => a-b;
-  }
+	constructor(){
+		this.arr = [];
+		this.compare = (a,b) => a-b;
+	}
 
-  add(element) 
-  {
-	this.arr.push(element)
-  }
+	add(element){
+		this.arr.push(element)
+	}
 
-  at(index) 
-  {
-    return this.arr[index]
-  }
+	at(index){
+		return this.arr[index]
+	}
 
-  get length() 
-  {
-    return this.arr.length
-  }
+	get length(){
+		return this.arr.length
+	}
 
-  toArray() 
-  {
-    return this.arr.slice()
-  }
+	toArray(){
+		return this.arr.slice()
+	}
 
-  sort(indices) 
-  {
-      let subArray = indices.sort().map((index) => this.arr[index]);
-      subArray.sort(this.compare);
-      this.arr.reduce((previousValue, currentItem, index, arr) => 
-	  {
-          if(indices.indexOf(index) > -1)
-			  {
-				arr[index] = subArray[indices.indexOf(index)]
-			  }
-          return arr
-      }, null);
-      return this
-  }
-
-  setComparator(compareFunction) 
-  {
-    this.compare = compareFunction;
-  }
+	sort(indices){
+		let subArray = indices.sort().map((index) => this.arr[index]);
+		subArray.sort(this.compare);
+		this.arr.reduce((previousValue, currentItem, index, arr) =>{
+			if(indices.indexOf(index) > -1){
+			arr[index] = subArray[indices.indexOf(index)]}
+			return arr}, null);
+		return this
+	}
+	
+	setComparator(compareFunction){
+		this.compare = compareFunction;
+	}
 }
 module.exports = Sorter;
